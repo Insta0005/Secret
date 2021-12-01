2a5cef383eecb02bef0ebc83fdf29432a1761b93
insta <instaworking5@gmail.com>
2021-12-01 19:17:11 +0530

    hashing & salting using bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
 
 const app = express();
 
@@ -21,8 +23,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});;
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
@@ -36,9 +36,12 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
+
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: hash
     });
 
     newUser.save(function(err){
@@ -59,7 +62,7 @@ app.post("/login", function(req, res){
             console.log(err);
         } else {
             if(foundUser){
-                if(foundUser.password === password) {
+                if(bcrypt.compareSync(password, foundUser.password)){
                     res.render("secrets");
                 }
             }
